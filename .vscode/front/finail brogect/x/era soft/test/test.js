972556730e264fc2e2de3a5fdb0c33dd5e02e51f
yward77 <yazanward8@gmail.com>
2025-08-30 22:46:20 +0300

    إضافة أيقونة الموقع وتعديل الكروت

---
+++ .vscode/front/finail brogect/x/era soft/test/test.js
@@ -0,0 +1,11 @@
+
+//let price =prompt(`price`) ;
+let fatora = prompt(`enter fatora price`)
+let discouint =prompt(`enter our discount`)
+let discount = fatora * ( discouint/100);
+console.log(`the price: `   +fatora)
+console.log(`discouint value is:`+ discount)
+let final = fatora - discount
+let daryba =14/100*final
+console.log( daryba+final)
+console.log(`final price is : ` +final) 
